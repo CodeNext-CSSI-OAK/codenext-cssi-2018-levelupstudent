6d3d779d685745d9f516eb9e62e735eb71614f44
Student <student@Admins-MacBook-Air.local>
2018-07-12 10:57:01 -0700

    Haunted House is not finished yet.

--- haunted-house.js
+++ haunted-house.js
@@ -1,19 +1,21 @@
-// Author: FirstName LastName
-var readline = require("readline-sync");
-
+// Jailyn Washington
+let readline = require("readline-sync");
 console.log();
 console.log("*****************************************************************");
 console.log("                          HAUNTED HOUSE");
-console.log("                 A game by FirstName LastName");
+console.log("                 A game by Jailyn Washington");
 console.log("*****************************************************************");
 console.log("You are standing in front of a spooky haunted house.");
 console.log("You hear what sounds like screaming coming from inside the house.");
-var enterHouse = readline.question("Will you enter the house? (yes or no) ");
-if(enterHouse == "y" || enterHouse == "yes") {
-  // your code here
-
-} else {
-  // your code here
-
+let enterHouse = readline.question("Will you enter the house? (yes or no) ");
+if(enterHouse === "y" || enterHouse === "yes") {
+  console.log(" You quickly go into the house the door slams shut and you immediately see a ghost")
+  let run = readline.question(" Do you run? ( yes or no) ");
+  if(run === "n" || run === "no") {
+    console.log(" The ghost looks at you but eventually disappears into a wall ");
+  } else {
+    if(run === "y" || run === "yes"){
+    console.log(" You were too scared. END OF GAME");
 }
-console.log("Thanks for playing!");
+ }
+  }
